d4a66f9f819c3c1b9a4202ef382620fd86d6ad35
henok Gebremedhn <henoklwi@gmail.com>
2019-10-29 22:58:40 -0700

    make change on main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-var 
+var name = prompt('What is your name?');
+var age = prompt("am i over 40?");
+age = age.toLowerCase();
+if (age === 'yes') {
+  alert('nop i am still young');
+}
+prompt('Do i leave in US?');
+prompt('Am i married?');
+prompt('Do i have children?')
+alert('Thank you for participating!!!')
