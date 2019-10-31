110938bd7b5ae5718874e1f9091cc0c2e32f54a3
henok Gebremedhn <henoklwi@gmail.com>
2019-10-31 07:14:36 -0700

    codefellows-day-2-lab

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
-var name = prompt('What is your name?');
-var age = prompt("am i over 40?");
-age = age.toLowerCase();
-if (age === 'yes') {
-  alert('nop i am still young');
+prompt('What is your name?');
+//question 1
+var age = prompt("Am i over 40?").toLowerCase();
+console.log('age', age)
+if (age === 'no' || age === 'n') {
+  alert('you are right');
 }
-prompt('Do i leave in US?');
-prompt('Am i married?');
-prompt('Do i have children?')
-alert('Thank you for participating!!!')
+else {
+  alert('nop i am still young!');
+}
+//question 2
+var place = prompt('Do i leave in US?').toLocaleLowerCase();
+if (place === 'yes' || place === 'y') {
+  alert('yes you got it!!');
+}
+else {
+  alert('you are missing seattle!!');
+}
+// question 3
+var status = prompt('Am i married?').toLowerCase();
+if (status === 'no' || status === 'n') {
+  alert('still looking the right one');
+}
+else {
+  alert('nope not yet!!');
+}
+//question 4
+var kids = prompt('Do i have children?').toLowerCase();
+if (kids === 'no' || kids === 'n') {
+  alert('you got it');
+}
+else {
+  alert('not yet!!');
+}
+alert('Thank you for participating!!');
