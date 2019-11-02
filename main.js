8dfe07a245c3a5d5e0d46bb203436d38533fcee5
Jordan Fox <jordanmichaelfox@hotmail.com>
2019-11-02 16:26:28 -0700

    Driver Jordan Nav Henok fix question 6

--- main.js
+++ main.js
@@ -1,54 +1,62 @@
-prompt('What is your name?');
-//question 1
-var age = prompt("Am i over 40?").toLowerCase();
-//console.log('age', age);
-if (age === 'no' || age === 'n') {
-  alert('you are right');
-}
-else {
-  alert('nop i am still young!');
-}
-//question 2
-var place = prompt('Do i leave in US?').toLocaleLowerCase();
-//console.log('yes', place);
-if (place === 'yes' || place === 'y') {
-  alert('yes you got it!!');
-}
-else {
-  alert('you are missing seattle!!');
-}
-// question 3
-var status = prompt('Am i married?').toLowerCase();
-//console.log('still looking ', status);
-if (status === 'no' || status === 'n') {
-  alert('still looking the right one');
-}
-else {
-  alert('nope not yet!!');
-}
-//question 4
-var kids = prompt('Do i have children?').toLowerCase();
-console.log('you got it', kids);
-if (kids === 'no' || kids === 'n') {
-  alert('you got it');
-}
-else {
-  alert('not yet!!');
-}
+'use strict';
+// prompt('What is your name?');
+// // //question 1
+// var age = prompt("Am i over 40?").toLowerCase();
+// //console.log('age', age);
+// if (age === 'no' || age === 'n') {
+//   alert('you are right');
+// }
+// else {
+//   alert('nop i am still young!');
+// }
+// // //question 2
+// var place = prompt('Do i leave in US?').toLocaleLowerCase();
+// //console.log('yes', place);
+// if (place === 'yes' || place === 'y') {
+//   alert('yes you got it!!');
+// }
+// else {
+//   alert('you are missing seattle!!');
+// }
+// // // question 3
+// var status = prompt('Am i married?').toLowerCase();
+// //console.log('still looking ', status);
+// if (status === 'no' || status === 'n') {
+//   alert('still looking the right one');
+// }
+// else {
+//   alert('nope not yet!!');
+// }
+// // //question 4
+// var kids = prompt('Do i have children?').toLowerCase();
+// console.log('you got it', kids);
+// if (kids === 'no' || kids === 'n') {
+//   alert('you got it');
+// }
+// else {
+//   alert('not yet!!');
+// }
 
-// question 6
-var favoritenum = prompt('guess my favorite number?');
+// // question 6
 
-for (let i = 1; i <= 4; i++); {
+function questionSix(){
 
-  if (favoritenum === 7) {
-    alert('yea how do you know it!!');
+  for (let i = 1; i <= 4; i++) {
+    var favoriteNum = prompt('guess my favorite number?');
+    console.log(favoriteNum);
+
+    if (favoriteNum === '7') {
+      console.log('hello');
+      alert('yea how do you know it!!');
+    }
+    else if (favoriteNum > 7) {
+      alert('too high');
+    }
+    else if (favoriteNum < 7) {
+      alert('too low');
+    }
   }
-  if (favoritenum > 7) {
-    alert('too high');
-  }
-  if (favoritenum < 7) {
-    alert('too low');
-  }
+}
 
-//alert('Thank you for participating!!');
+questionSix();
+alert('Thank you for participating!!');
